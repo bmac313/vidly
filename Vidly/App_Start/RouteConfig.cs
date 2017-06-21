e558fe7ccca9bfcea5232db3e6527478bd45a902
Brendan <mcb0193@comcast.net>
2017-06-21 18:07:21 -0500

    Change references to "id" to "movieId"

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace Vidly
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}/{movieId}",
+                defaults: new { controller = "Home", action = "Index", movieId = UrlParameter.Optional }
             );
         }
     }
